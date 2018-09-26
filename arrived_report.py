f593148ab115b84ebc39a91c0313da591d014066
Randhawa <randhawa.harpreet@live.com>
2018-09-26 18:36:07 +0530

    new changes

--- arrived_report.py
+++ arrived_report.py
@@ -3,9 +3,6 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
-
-
-
 from email import encoders
 import datetime
 from Scripts.Connection import mysql_server
